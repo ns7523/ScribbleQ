526fb73a8e2c43d3b16e867d90e7ba1e96808553
N S AKASH <nsakash752003@gmail.com>
2026-02-01 02:30:40 +0530

    Update Streamlit page configuration settings

--- demo/app.py
+++ demo/app.py
@@ -17,16 +17,16 @@ import logging
 logging.getLogger().setLevel(logging.ERROR)
 forward_device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-st.set_page_config(
-    page_title="ScribbleQ",
-    page_icon="🚀",
-)
 
 def main(det_archs, reco_archs):
     """Build a streamlit layout"""
     # Wide mode
-    
-    st.set_page_config(layout="wide")
+    st.set_page_config(
+        page_title="ScribbleQ",
+        page_icon="✍️",
+        layout="wide",
+    )
+    #st.set_page_config(layout="wide")
 
     # Designing the interface
     st.title("ScribbleQ ")
