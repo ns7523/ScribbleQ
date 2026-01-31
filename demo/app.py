fc8cb2ff4d34e71f3a8b4eaf0c2155239750f239
N S AKASH <nsakash752003@gmail.com>
2026-02-01 01:07:25 +0530

    Add DOCTR_FONT environment variable for font path
    Set the DOCTR_FONT environment variable to the DejaVuSans.ttf font.

--- demo/app.py
+++ demo/app.py
@@ -4,6 +4,7 @@
 # See LICENSE or go to <https://opensource.org/licenses/Apache-2.0> for full license details.
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+os.environ["DOCTR_FONT"] = os.path.join(os.path.dirname(__file__), "DejaVuSans.ttf")
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
